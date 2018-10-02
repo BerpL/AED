6d7c0a41141ecfe2f1a334db1e8cd0223e06fcf5
bugstyle <eder.cornejo.diaz@ucsp.edu.pe>
2018-10-02 10:21:06 -0500

    Updated

---
+++ Linkedlist2.cpp
@@ -0,0 +1,92 @@
+// Linkedlist2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Cnode {
+public:
+	Cnode(T x)
+	{
+		m_data = x;
+		m_next = 0;
+	};
+	T m_data;
+	Cnode<T>*m_next;
+};
+
+template<class T>
+class Clist {
+public:
+	Clist() {
+		m_head = 0;
+	}
+	bool find(T x, Cnode<T>**&p);
+	bool insert(T x);
+	bool remove(T x);
+	bool print();
+
+	Cnode<T>*m_head;
+};
+
+template<class T>
+bool Clist<T>::find(T x, Cnode<T>**&p) {
+	for (p = &m_head; *p && (*p)->m_data < x; p = &((*p)->m_next));
+	return (*p) && (*p)->m_data == x;
+}
+
+template<class T>
+bool Clist<T>::insert(T x) {
+	Cnode<T>**p;
+	if (find(x, p))return 0;
+	Cnode<T>*t = new Cnode<T>(x);
+	t->m_next = *p;
+	*p = t;
+	return 1;
+}
+
+template<class T>
+bool Clist<T>::remove(T x) {
+	Cnode<T>**p;
+	if (!find(x, p))return 0;
+	Cnode<T>*t = *p;
+	*p = t->m_next;
+	delete t;
+	return 1;
+}
+
+template<class T>
+bool Clist<T>::print() {
+	cout << "head-> ";
+	for(Cnode<T>**p=&m_head;*p; p = &((*p)->m_next))
+		cout << (*p)->m_data << " -> ";
+	cout << " NULL";
+	return 1;
+}
+
+int main() {
+	Clist<int> list;
+	list.insert(3);
+	list.insert(5);
+	list.insert(1);
+	list.insert(7);
+	list.insert(9);
+	list.insert(2);
+	list.remove(1);
+	list.remove(9);
+	list.print();
+	return 0;
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
